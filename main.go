62a1f39b632fa3a64ac5d5b10d2dff739b4282d4
lijunlong <lijunlong@openresty.com>
2021-04-25 16:11:34 +0800

    rewrite count data.

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 func main() {
 	// Load the database we wish to enrich.
-	writer, err := mmdbwriter.Load("GeoLite2-Country.mmdb", mmdbwriter.Options{})
+	// writer, err := mmdbwriter.Load("GeoLite2-Country.mmdb", mmdbwriter.Options{})
+	writer, err := mmdbwriter.Load("GeoLite2-City.mmdb", mmdbwriter.Options{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -64,8 +65,36 @@ func main() {
 		log.Fatal(err)
 	}
 
+	_, city, err := net.ParseCIDR("110.87.70.227/32")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//"country": {
+	//	"geoname_id": 1814991,
+	//	"iso_code": "CN",
+	//	"names": {
+	//		"en": "China",
+	//		"ja": "中国",
+	//		"zh-CN": "中国"
+	//	}
+	//},
+	cityData := mmdbtype.Map{
+		"country": mmdbtype.Map{
+			"geoname_id": mmdbtype.Uint32(1814991),
+			"iso_code":   mmdbtype.String("CN"),
+			"names": mmdbtype.Map{
+				"en":    mmdbtype.String("China"),
+				"zh-CN": mmdbtype.String("中国"),
+			},
+		},
+	}
+	if err := writer.InsertFunc(city, inserter.TopLevelMergeWith(cityData)); err != nil {
+		log.Fatal(err)
+	}
+
 	// Write the newly enriched DB to the filesystem.
-	fh, err := os.Create("GeoLite2-Country-with-Department-Data.mmdb")
+	fh, err := os.Create("GeoLite2-City-with-Department-Data.mmdb")
 	if err != nil {
 		log.Fatal(err)
 	}
